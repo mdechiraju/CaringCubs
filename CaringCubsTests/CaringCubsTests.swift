1252c197d6970703a58c0893834f7e52b5934ec1
Murali D <dmurali_24@yahoo,com>
2026-03-30 09:40:13 -0500

    Initial Commit

---
+++ CaringCubsTests/CaringCubsTests.swift
@@ -0,0 +1,19 @@
+//
+//  CaringCubsTests.swift
+//  CaringCubsTests
+//
+//  Created by Murali on 3/30/26.
+//
+
+import Testing
+@testable import CaringCubs
+
+struct CaringCubsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
